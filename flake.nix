71296e1e510b20085c35bec5e8a887b35d4d0acd
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-02-23 21:42:30 +0100

    wrap nix shell in flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+# flake.nix
+{
+  description = "Arduino-IDE, uses unstable";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs}: 
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+
+      devShell.x86_64-linux =
+        pkgs.mkShell { buildInputs = 
+          [         
+            pkgs.arduino-ide
+          ];
+
+        };
+   };
+}
